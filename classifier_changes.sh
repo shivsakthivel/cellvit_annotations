cfe6401812731b9c66fde261b6770734c4ce5260
shivsakthivel <62940654+shivsakthivel@users.noreply.github.com>
2025-11-18 14:39:28 +0000

    Modify paths in classifier_changes.sh to use placeholders
    Updated log file paths and project directories to use placeholders.

--- classifier_changes.sh
+++ classifier_changes.sh
@@ -4,12 +4,12 @@
 #SBATCH -t 12:00:00
 #SBATCH -p epyc
 #SBATCH -J classifier_changes
-#SBATCH -o /mnt/scratchc/fmlab/sakthi01/logs/classifier_changes.log
-#SBATCH -e /mnt/scratchc/fmlab/sakthi01/logs/classifier_changes.error
+#SBATCH -o <directory>/logs/classifier_changes.log
+#SBATCH -e <directory>/logs/classifier_changes.error
 
 conda run -n classifier_changes python process_changes.py \
 --num_workers 4 \
---detection_dir /mnt/scratchc/fmlab/sakthi01/classifier_training_project/cellvit_pp_outputs \
---segmentation_dir /mnt/scratchc/fmlab/sakthi01/classifier_training_project/cellvit_pp_outputs \
---annotation_dir /mnt/scratchc/fmlab/sakthi01/classifier_training_project/annotations \
---output_dir /mnt/scratchc/fmlab/sakthi01/classifier_training_project/processed_annotations
+--detection_dir <directory>/cellvit_pp_outputs \
+--segmentation_dir <directory>/cellvit_pp_outputs \
+--annotation_dir <directory>/annotations \
+--output_dir <directory>/processed_annotations
